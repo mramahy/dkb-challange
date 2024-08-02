bb0722d7de5d80cbb94c9b831049eba158cf9a79
muhammad.bekette <mramahy@fcih.net>
2024-08-02 11:19:53 +0200

    init dkb-challenge

---
+++ src/main/kotlin/com/dkb/dkbchallange/DkbChallangeApplication.kt
@@ -0,0 +1,11 @@
+package com.dkb.dkbchallange
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DkbChallangeApplication
+
+fun main(args: Array<String>) {
+    runApplication<DkbChallangeApplication>(*args)
+}
---
+++ src/test/kotlin/com/dkb/dkbchallange/DkbChallangeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dkb.dkbchallange
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DkbChallangeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
