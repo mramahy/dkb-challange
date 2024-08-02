e6bcba085bad4fa1c8a9687151a8045b6b458708
muhammad.bekette <mramahy@fcih.net>
2024-08-02 14:09:21 +0200

    refactor controller, make more readable and cleaner

--- src/main/kotlin/com/dkb/dkbchallange/rest/ShortUrlController.kt
+++ src/main/kotlin/com/dkb/dkbchallange/rest/ShortUrlController.kt
@@ -15,26 +15,34 @@ import java.net.URI
 @RequestMapping("/")
 class ShortUrlController(private val service: ShortUrlService) {
 
-    @GetMapping(value = ["/{id}"])
+    @GetMapping("/{id}")
     fun redirect(@PathVariable id: String): ResponseEntity<Unit> {
         val url = service.getShortUrl(id)
         return if (url != null) {
             ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
                 .header("Location", url)
                 .build()
-        }
-        else {
+        } else {
             ResponseEntity(HttpStatus.NOT_FOUND)
         }
     }
 
     @PostMapping
     fun create(@RequestParam("url") url: String): ResponseEntity<String> {
-        val uri = URI(url)
-        return if (uri.scheme == "http" || uri.scheme == "https") {
+        return if (isValidUrl(url)) {
             val shortUrl = service.saveUrl(url)
             ResponseEntity(shortUrl, HttpStatus.OK)
-        } else
+        } else {
             ResponseEntity(HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    private fun isValidUrl(url: String): Boolean {
+        return try {
+            val uri = URI(url)
+            uri.scheme == "http" || uri.scheme == "https"
+        } catch (e: Exception) {
+            false
+        }
     }
 }
